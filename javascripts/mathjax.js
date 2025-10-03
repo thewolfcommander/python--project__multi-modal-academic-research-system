3ed2795a592bb852ddc28f2a7378597916d4d0f1
Manoj Pawan Tyagi <tyagimanojtyagi.22@gmail.com>
2025-10-03 08:14:39 +0530

    Deployed 656459b with MkDocs version: 1.6.1

---
+++ javascripts/mathjax.js
@@ -0,0 +1,18 @@
+// MathJax Configuration for Multi-Modal Academic Research System Documentation
+
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise()
+})
